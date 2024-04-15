0cb760fb0a912537017896652adcf269563294f7
493165 <167090953+493165@users.noreply.github.com>
2024-04-15 14:41:04 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -28,8 +28,8 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
-    try: 
+
+    try:
         http = urllib.PoolManager()
     except:
         print('Exception')
@@ -64,4 +64,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
